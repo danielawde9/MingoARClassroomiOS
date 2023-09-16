54610d32c196dd4028a4bb5702919b9ff728ea74
danielawde <danielawde9@gmail.com>
2023-09-16 13:36:44 +0300

    scroll view not woking

--- MingoARClassroom/ARSceneWithMenu.swift
+++ MingoARClassroom/ARSceneWithMenu.swift
@@ -4,7 +4,8 @@ struct ARSceneWithMenu: View {
     var selectedPlanets: [String]
     
     @State private var isMenuShown = false
-    private let maxMenuHeight: CGFloat = 300  // Set max height
+    private let rowHeight: CGFloat = 60
+    private let menuHeight: CGFloat = 400
     
     var body: some View {
         ZStack {
@@ -12,62 +13,64 @@ struct ARSceneWithMenu: View {
             
             VStack {
                 Spacer()
-                Button(action: {
-                    withAnimation {
-                        isMenuShown.toggle()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        withAnimation {
+                            isMenuShown.toggle()
+                        }
+                    }) {
+                        Image(systemName: isMenuShown ? "chevron.down.circle.fill" : "chevron.up.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.white)
+                            .frame(width: 60, height: 60)
+                            .padding(15)
+                            .background(Color.black.opacity(0.6))
+                            .clipShape(Circle())
                     }
-                }) {
-                    Text(isMenuShown ? "chevron.up" : "chevron.down")
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 20)
-                        .background(Capsule().fill(Color.black.opacity(0.6)))
+                    .padding(.trailing, 20)
                 }
-                .padding(.bottom, 10)
-                
-                if isMenuShown {
-                    VisualEffectView(effect: UIBlurEffect(style: .dark))
-                        .clipShape(RoundedTopRectangle())
-                        .transition(.move(edge: .bottom))
-                        .overlay(
-                            VStack {
+                .padding(.bottom, 20)
+            }
+            
+            if isMenuShown {
+                VisualEffectView(effect: UIBlurEffect(style: .dark))
+                    .transition(.move(edge: .bottom))
+                    .overlay(
+                        ScrollView {
+                            
+                            VStack(spacing: 10) {
                                 Text("Selected Planets")
                                     .font(.title2)
                                     .fontWeight(.bold)
                                     .foregroundColor(.white)
-                                    .padding(.top, 10)
-
+                                
                                 List(selectedPlanets, id: \.self) { planet in
-                                    Text(planet)
-                                        .foregroundColor(.white)
-                                        .font(.headline)
+                                    HStack {
+                                        Image("\(planet)Icon")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 50, height: 50)
+                                        
+                                        Text(planet)
+                                            .foregroundColor(.white)
+                                            .font(.headline)
+                                    }
+                                    .frame(height: rowHeight)
                                 }
                                 .listStyle(PlainListStyle())
                                 .background(
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .fill(Color.white)
+                                    RoundedRectangle(cornerRadius: 20)
                                 )
-                                .clipShape(RoundedTopRectangle())
-                                .padding(.horizontal, 10)
                             }
-                        )
-                        .frame(maxWidth: .infinity, maxHeight: maxMenuHeight)  // Set max width and height
-                }
+                            .padding()
+                        }
+                    )
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .padding(.horizontal, 15)
+                    .frame(height: menuHeight)
             }
         }
-    }
-}
-
-struct RoundedTopRectangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: 0, y: rect.maxY))  // bottom left
-        path.addLine(to: CGPoint(x: 0, y: rect.minY + 20)) // top left with rounding
-        path.addArc(center: CGPoint(x: 20, y: rect.minY + 20), radius: 20, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
-        path.addLine(to: CGPoint(x: rect.maxX - 20, y: rect.minY))  // before top right rounding
-        path.addArc(center: CGPoint(x: rect.maxX - 20, y: rect.minY + 20), radius: 20, startAngle: Angle(degrees: 270), endAngle: Angle(degrees: 360), clockwise: false)
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))  // bottom right
-        return path
     }
 }
