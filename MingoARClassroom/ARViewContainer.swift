e7782c13233b0bd68c3f44790338777e54be1a92
danielawde <danielawde9@gmail.com>
2023-09-17 13:55:48 +0300

    few fixes bas meh zabt la hon

--- MingoARClassroom/ARViewContainer.swift
+++ MingoARClassroom/ARViewContainer.swift
@@ -3,7 +3,7 @@ import ARKit
 
 struct ARViewContainer: UIViewRepresentable {
     var selectedPlanets: [String]
-    private var planetData: [Planet] = loadPlanetData() // This function was not provided; please make sure it exists
+    private var planetData: [Planet] = loadPlanetData()
     private let planetCreator = ARPlanetCreator()
     
     var solarSystemNode: SCNNode = SCNNode()
@@ -113,8 +113,7 @@ struct ARViewContainer: UIViewRepresentable {
             
             for planet in parent.planetData where parent.selectedPlanets.contains(planet.name) {
                 
-                let orbitNode = planetCreator.orbitingNode(forPlanet: planet)
-                parent.solarSystemNode.addChildNode(orbitNode)
+               
             }
             
             node.addChildNode(parent.solarSystemNode)
@@ -140,59 +139,12 @@ class ARPlanetCreator {
 
         return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
-
-    func createOrbit(radius: CGFloat, colorHex: String) -> SCNNode {
-        let orbit = SCNTorus(ringRadius: radius, pipeRadius: 0.0002)
-        let material = SCNMaterial()
-        material.diffuse.contents = hexStringToUIColor(hex: colorHex) // Adjusted this line
-        orbit.materials = [material]
-
-        let orbitNode = SCNNode(geometry: orbit)
-        return orbitNode
-    }
     
-    func orbitingNode(forPlanet planetInfo: Planet) -> SCNNode {
-        let orbitNode = SCNNode()
-
-        // Convert planet's distanceFromSun to an appropriate scale
-        let distanceFromSunInScene = CGFloat(planetInfo.distanceFromSun) / 1000.0
-
-        // Create and add visual orbit ring
-        let orbitRing = createOrbit(radius: distanceFromSunInScene, colorHex: planetInfo.planetColor)
-        orbitNode.addChildNode(orbitRing)
-
-        // Create revolution node
-        let revolutionNode = SCNNode()
-        revolutionNode.position = SCNVector3(0, 0, 0)  // Center the revolution node
-        revolutionNode.pivot = SCNMatrix4MakeTranslation(-Float(distanceFromSunInScene), 0, 0)  // Move the pivot away by the radius
-
-        // Create planet and add to the revolution node
-        if let planetNode = createARPlanet(name: planetInfo.name, data: [planetInfo]) {
-            revolutionNode.addChildNode(planetNode)
-        }
-
-        orbitNode.addChildNode(revolutionNode)
-
-        // Orbit around the Y-axis of the Sun
-        let orbitalDuration = planetInfo.orbitalPeriod / 10.0
-        print("Starting revolution of \(planetInfo.name) with a duration of \(orbitalDuration) seconds.")
-
-        let orbitAction = SCNAction.repeatForever(SCNAction.rotate(by: .pi * 2, around: SCNVector3(0, 1, 0), duration: orbitalDuration))
-        revolutionNode.runAction(orbitAction)
-
-        // Apply inclination
-        orbitNode.rotation = SCNVector4(1, 0, 0, Float(planetInfo.orbitalInclination).toRadians())
-        orbitNode.eulerAngles.x = Float(planetInfo.orbitalInclination).toRadians()
-
-        return orbitNode
-    }
-
-
-
     func createARPlanet(name: String, data: [Planet]) -> SCNNode? {
         guard let planetInfo = data.first(where: { $0.name == name }) else { return nil }
 
-        let diameter = CGFloat(planetInfo.diameter) / 1_000_000.0
+        // Convert planet's diameter to an appropriate scale for AR
+        let diameter = CGFloat(planetInfo.diameter)
         let planet = SCNSphere(radius: diameter / 2)
         planet.segmentCount = 550
         
@@ -202,12 +154,6 @@ class ARPlanetCreator {
         
         let planetNode = SCNNode(geometry: planet)
         planetNode.name = planetInfo.name
-
-        // Planet self rotation
-        let selfRotationDuration = planetInfo.rotationPeriod / 10.0
-        
-        let selfRotationAction = SCNAction.repeatForever(SCNAction.rotate(by: .pi * 2, around: SCNVector3(0, 1, 0), duration: selfRotationDuration))
-        planetNode.runAction(selfRotationAction)
 
         return planetNode
     }
--- MingoARClassroom/PlanetModel.swift
+++ MingoARClassroom/PlanetModel.swift
@@ -1,24 +1,28 @@
 import Foundation
 
-struct Planet:Identifiable, Decodable {
+struct Planet: Identifiable, Decodable {
     var id: String { name }
     let name: String
     let diameter: Double
-    let rotationPeriod: Double
+    var rotationPeriod: Double
     let lengthOfDay: Double
-    let distanceFromSun: Double
-    let perihelion: Double
-    let aphelion: Double
-    let orbitalPeriod: Double
+    var distanceFromSun: Double
+    var perihelion: Double
+    var aphelion: Double
+    var orbitalPeriod: Double
     let orbitalVelocity: Double
     let orbitalInclination: Double
     let orbitalEccentricity: Double
     let obliquityToOrbit: Double
-    let prefabName: String
     let planetColor: String
     let moons: [Moon]
     let rings: [Ring]
     let pointsOfInterest: [PointOfInterest]?
+    var orbitalEccentricitySquared: Double { orbitalEccentricity * orbitalEccentricity }
+    var orbitProgress: Float
+    var rotationProgress: Float
+    var completedOrbits: Int
+    var completedSelfRotations: Int
 }
 
 struct Moon: Identifiable, Decodable {
@@ -29,14 +33,12 @@ struct Moon: Identifiable, Decodable {
     let lengthOfDay: Double
     let distanceFromPlanet: Double
     let orbitalPeriod: Double
-    let prefabName: String
 }
 
 struct Ring: Decodable {
     let name: String
     let innerRadius: Double
     let outerRadius: Double
-    let prefabName: String
 }
 
 struct PointOfInterest: Decodable {
@@ -54,8 +56,19 @@ func loadPlanetData() -> [Planet] {
 
     do {
         let data = try Data(contentsOf: url)
-        let decodedData = try JSONDecoder().decode([String: [Planet]].self, from: data)
-        return decodedData["planets"] ?? []
+        var decodedData = try JSONDecoder().decode([String: [Planet]].self, from: data)
+        guard var planets = decodedData["planets"] else { return [] }
+        
+        // Applying the conversions
+        for (index, planet) in planets.enumerated() {
+            planets[index].rotationPeriod *= 3600 // Convert hours to seconds
+            planets[index].orbitalPeriod *= 86400 // Convert days to seconds
+            planets[index].perihelion *= 1e6 // Convert 10^6 km to km
+            planets[index].aphelion *= 1e6 // Convert 10^6 km to km
+            planets[index].distanceFromSun *= 1e6 // Convert 10^6 km to km
+        }
+        print(planets)
+        return planets
     } catch {
         print("Error decoding JSON: \(error)")
         return []
