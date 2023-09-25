5ec2d5230bd44f90638c68a9551df4d80d4e5d29
danielawde <danielawde9@gmail.com>
2023-09-25 19:37:41 +0300

    ui fixes

--- MingoARClassroom/Views/ARPlaceView.swift
+++ MingoARClassroom/Views/ARPlaceView.swift
@@ -4,18 +4,17 @@ struct ARPlaceView: View {
     var selectedPlanets: [String]
     
     @State private var isMenuShown = false
-    @State private var sliderValue: Double = 0.5 // Example state for the slider
-    @State private var isToggleOn: Bool = true // Example state for the toggle
+    @State private var sliderValue: Double = 0.5
+    @State private var isToggleOn: Bool = true
     
     private let rowHeight: CGFloat = 60
-    private let menuHeight: CGFloat = 400
+    private let navigationBarHeight: CGFloat = 44
+    private let buttonSize: CGFloat = 50
     
     var body: some View {
         ZStack {
             ARViewContainer(selectedPlanets: selectedPlanets)
-            
             VStack {
-                Spacer()
                 HStack {
                     Spacer()
                     Button(action: {
@@ -23,60 +22,78 @@ struct ARPlaceView: View {
                             isMenuShown.toggle()
                         }
                     }) {
-                        Image(systemName: isMenuShown ? "chevron.down.circle.fill" : "chevron.up.circle.fill")
-                            .resizable()
-                            .scaledToFit()
+                        Image(systemName: "list.bullet")
                             .foregroundColor(.white)
-                            .frame(width: 60, height: 60)
-                            .padding(15)
-                            .background(Color.black.opacity(0.6))
-                            .clipShape(Circle())
+                            .font(.system(size: 24)) // Adjust the size of the image.
+                            .frame(width: buttonSize, height: buttonSize) // Explicit frame for consistent size
+                            .background(Color.black.opacity(0.5)) // A bit more transparency to make it more like Apple's design
+                            .clipShape(Circle()) // Makes the button round
                     }
+                    .padding(.top, navigationBarHeight - 15)
                     .padding(.trailing, 20)
+
                 }
-                .padding(.bottom, 20)
+                Spacer()
             }
-            
+
             if isMenuShown {
-                VisualEffectView(effect: UIBlurEffect(style: .dark))
-                    .transition(.move(edge: .bottom))
-                    .overlay(
-                        VStack(spacing: 10) {
-                            Text("Selected Planets")
-                                .font(.title2)
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                            
-                            Slider(value: $sliderValue, in: 0...1)
-                                .accentColor(.white)
-                            
-                            Toggle("Toggle Label", isOn: $isToggleOn)
-                                .toggleStyle(SwitchToggleStyle())
-                            
-                            List(selectedPlanets, id: \.self) { planet in
-                                HStack {
-                                    Image("\(planet)Icon")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 50, height: 50)
-                                    
-                                    Text(planet)
-                                        .foregroundColor(.white)
-                                        .font(.headline)
+                VStack {
+                    
+                    Spacer()  // This will push the contents of the VStack downwards.
+                    
+                    VStack {
+                        Capsule()
+                            .frame(width: 40, height: 5)
+                            .foregroundColor(.gray)
+                            .padding(8)
+                        
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                withAnimation {
+                                    isMenuShown = false
                                 }
-                                .frame(height: rowHeight)
+                            }) {
+                                Image(systemName: "xmark")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .foregroundColor(.white)
+                                    .frame(width: 20, height: 20)
                             }
-                            .listStyle(PlainListStyle())
-                            .background(
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(Color.clear)
-                            )
+                            .padding(.trailing, 20)
                         }
-                            .padding()
-                    )
-                    .clipShape(RoundedRectangle(cornerRadius: 25))
-                    .padding(.horizontal, 15)
-                    .frame(height: menuHeight)
+                        
+                        Text("Selected Planets")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        
+                        Slider(value: $sliderValue, in: 0...1)
+                            .accentColor(.white)
+                        
+                        Toggle("Toggle Label", isOn: $isToggleOn)
+                            .toggleStyle(SwitchToggleStyle())
+                        
+                        List(selectedPlanets, id: \.self) { planet in
+                            HStack {
+                                Image("\(planet)Icon")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                
+                                Text(planet)
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                            }
+                            .frame(height: rowHeight)
+                        }
+                        .listStyle(PlainListStyle())
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.7).cornerRadius(25))
+                    .transition(.move(edge: .bottom))
+                    .frame(maxHeight: UIScreen.main.bounds.height / 2)
+                }
             }
         }
     }
